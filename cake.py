e8a1ed0c949768833d66a4fec5a5b406c75f9958
micahculpepper <micahculpepper@gmail.com>
2016-09-30 12:06:40 -0500

    initial commit

---
+++ cake.py
@@ -0,0 +1,42 @@
+print ("""\x1b[31;1m
+Festively crouched at the starting line
+Reindeer pumping and thumping in time
+The red nose flashes, the flags go up
+Churning and burning, they yearn for the cup
+\x1b[32;1m
+Dasher maneuvers and muscles for rank
+Blitzen burning fast on an empty tank
+Reckless and wild, they pour through the turns
+Their prowess is potent and secretly stern
+\x1b[31;1m
+As they speed 'cross the houses, the lights flash red
+The kids get up and they get out of bed
+The night is empty except for one man
+Still driving and striving as fast as he can
+\x1b[32;1m
+The sun has gone down and the moon has come up
+And long ago Santa Claus left with their stuff
+But he's driving and striving and hugging the turns
+And thinking of children for whom he still burns
+\x1b[31;1m
+He's going the distance, he's going for speed
+To all the homes, all the homes in their time of need
+Because he's racing and pacing and saving the day
+He's fighting and gliding and riding on his sleigh
+He's going the distance
+\x1b[32;1m
+No cookies, no fruitcake, no eggnog, no wine
+He's heartened by something he cannot define
+Belly-shaking earthquakes of laughter and joy
+Regale them, assail them with monster-truck toys
+\x1b[31;1m
+In his mind, he's still driving, still making them play
+He's hoping in time that their memories will stay
+'Cause he's racing and pacing and saving the day
+He's fighting and gliding and riding on his sleigh
+\x1b[32;1m
+The sun has gone down and the moon has come up
+And long ago Santa Claus left with their stuff
+But he's driving and striving and hugging the turns
+And thinking of children for whom he still burns
+\x1b[0m""")
